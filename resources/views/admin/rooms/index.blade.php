f2920b8d513d49b896686585a9977ac46183219b
nhquan21 <132729291+nhquan21@users.noreply.github.com>
2023-11-17 16:30:09 +0700

    17/11

--- resources/views/admin/rooms/index.blade.php
+++ resources/views/admin/rooms/index.blade.php
@@ -54,7 +54,16 @@
                             <table id="example" class="display table-hover table-responsive-xl listing">
                                 <thead>
                                     <tr>
+<<<<<<< Updated upstream
+                                        <th>Mã phòng</th>
+=======
+                                        <th>
+                                            <div class="form-check">
+                                                <input class="form-check-input" id="master-checkbox" onclick="checkbox()" type="checkbox">
+                                            </div>
+                                        </th>
                                         <th>STT</th>
+>>>>>>> Stashed changes
                                         <th>Img</th>
                                         <th>Hạng phòng</th>
                                         <th>Tên phòng</th>
@@ -70,19 +79,50 @@
                                 <tbody>
                                     @forelse ($rooms as $item)
                                         <tr>
-                                            <td>{{ $loop->iteration }}</td>
+<<<<<<< Updated upstream
+                                            <td>{{ $item->room_code }}</td>
                                             <td><img src="{{ asset('storage/images/' . $item->image) }}" alt="table-img"
                                                 class="img-fluid rounded-circle" width="40px"></td>
                                                 <td>{{ $item->typeRooms->room_type }}</td>
                                                 <td>{{ $item->name }}</td>
-                                                <td>{{ $item->room_code }}</td>
                                                 <td>{{ number_format($item->price) }}/night</td>
                                                 <td>{{ $item->adluts }}</td>
                                                 <td>{{ $item->children }}</td>
-                                                <td class="{{ $item->isBooked ? 'active' : 'draft' }}">
-                                                    <span>{{ $item->isBooked ? 'On' : 'Off' }}</span>
+                                                <td >
+                                                    @if (isset($status))
+                                                        @if ($status == 0)
+                                                        <span>Đã đặt</span>        
+                                                        @else
+                                                        <span>{{ $item->status ? 'Còn trống' : 'Đã đặt' }}</span>        
+                                                        @endif
+                                                    
+                                                    @else
+                                                    
+                                                    @endif
+                                                    
                                                 </td>
                                                 <td>{{$item->created_at}}</td>
+=======
+                                            <td>
+                                                <div class="form-check">
+                                                    <input class="form-check-input de " name="sub" type="checkbox"
+                                                        id="{{ $item->id }}">
+                                                </div>
+                                            </td>
+                                            <td>{{ $loop->iteration }}</td>
+                                            <td><img src="{{ asset('storage/images/' . $item->image) }}" alt="table-img"
+                                                    class="img-fluid rounded-circle" width="40px"></td>
+                                            <td>{{ $item->typeRooms->room_type }}</td>
+                                            <td>{{ $item->name }}</td>
+                                            <td>{{ $item->room_code }}</td>
+                                            <td>{{ number_format($item->price) }}/night</td>
+                                            <td>{{ $item->adluts }}</td>
+                                            <td>{{ $item->children }}</td>
+                                            <td class="{{ $item->isBooked ? 'active' : 'draft' }}">
+                                                <span>{{ $item->isBooked ? 'On' : 'Off' }}</span>
+                                            </td>
+                                            <td>{{ $item->created_at }}</td>
+>>>>>>> Stashed changes
                                             <td>
                                                 <a href="{{ route('rooms.edit', $item) }}"><i
                                                         class="fas fa-edit"></i></a>
